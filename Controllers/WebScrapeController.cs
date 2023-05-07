a91779e1d8f7cfbce99a85421d9edd6af2972ee5
traverspolius <64287688+traverspolius@users.noreply.github.com>
2023-05-07 01:42:49 -0700

    Add project files.

---
+++ Controllers/WebScrapeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using HtmlAgilityPack;
+using TrowWebScraper.Models;
+
+namespace TrowWebScraper.Controllers
+{
+    public class WebScrapeController : Controller
+    {
+        // GET: Now Playing Movies
+        public IActionResult Index()
+        {
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            List<NowPlayingMovies> movies = new List<NowPlayingMovies>();
+
+            var web = new HtmlWeb();
+            var doc = web.Load("https://www.landmarkcinemas.com/now-playing/new-westminster#");
+            foreach(var item in doc.DocumentNode.SelectNodes("//li[@class='movieitem']"))
+            {
+                string title = item.SelectSingleNode(".//span[@class='filmListItemTitle']").InnerText.Trim();
+
+                movies.Add(new NowPlayingMovies()
+                {
+                    title = title
+                });
+            }
+            return View(movies);
+        }
+    }
+}
---
+++ Models/NowPlayingMovies.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrowWebScraper.Models
+{
+    public class NowPlayingMovies
+    {
+        public string title { get; set; }
+        //public string details { get; set; }
+        //public string image { get; set; }
+    }
+}
