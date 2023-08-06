3ae86ccc4e7e77727eb6f00d550738ce34560423
Travers Polius <traverse.polius@outlook.com>
2023-08-06 00:23:11 -0700

    ver20230806

--- Controllers/WebScrapeController.cs
+++ Controllers/WebScrapeController.cs
@@ -11,7 +11,13 @@ namespace TrowWebScraper.Controllers
 {
     public class WebScrapeController : Controller
     {
-        // GET: Now Playing Movies
+        private const string URL = "https://www.landmarkcinemas.com/now-playing/new-westminster#";
+        private const string TOP_NODE = "//li[@class='movieitem']";
+        private const string SUB_NODE_A = ".//img";
+        private const string ATTR_VALUE = "data-original"; // Attribute Value
+        private const string SUB_NODE_B = ".//span[@class='filmListItemTitle']";
+
+        // GET: Now Playing Movies1
         public IActionResult Index()
         {
             ServicePointManager.Expect100Continue = true;
@@ -20,17 +26,21 @@ namespace TrowWebScraper.Controllers
             string imagex = "";
             string titlex = "";
             var web = new HtmlWeb();
-            var doc = web.Load("https://www.landmarkcinemas.com/now-playing/new-westminster#");
-            foreach(var item in doc.DocumentNode.SelectNodes("//li[@class='movieitem']"))
+            var doc = web.Load(URL);
+            foreach(var item in doc.DocumentNode.SelectNodes(TOP_NODE))
             {
-                imagex = item.SelectSingleNode(".//img").GetAttributeValue("data-original", null);
-                titlex = item.SelectSingleNode(".//span[@class='filmListItemTitle']").InnerText.Trim();
+                imagex = item.SelectSingleNode(SUB_NODE_A).GetAttributeValue(ATTR_VALUE, null);
+                titlex = item.SelectSingleNode(SUB_NODE_B).InnerText.Trim();
 
-                movies.Add(new NowPlayingMovies()
+                if (imagex is not null)
                 {
-                    image = imagex,
-                    title = titlex,
-                });
+                    movies.Add(new NowPlayingMovies()
+                                {
+                                    image = imagex,
+                                    title = titlex,
+                                });
+                }
+                
             };
             return View(movies);
         }
